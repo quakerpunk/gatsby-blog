631eaf8cc358f9f2ca51a41912e187b92c9e24f1
Shawn Borton <shawn@shawnborton.info>
2020-04-01 23:34:39 -0500

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Just Some Dude`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Shawn Borton`,
+      summary: `who lives and works near Dallas and builds useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `QuakerPunk`,
     },
   },
   plugins: [
